209edd33c7838e72ee0632210bae879831f19fce
SmithEcophysLab <nick.gregory.smith@gmail.com>
2020-02-18 12:53:02 -0600

    finalized sla data entry and added it to the analysis

--- analysis/springlake_2019_analysis.R
+++ analysis/springlake_2019_analysis.R
@@ -2,6 +2,7 @@
 
 ## libraries
 library(tidyr)
+library(dplyr)
 library(emmeans)
 
 ## plot information
@@ -18,9 +19,23 @@ hist(lai$lai_mean)
 
 lai_lm = lm(lai_mean ~ variety * plantingfac, data = lai)
 anova(lai_lm)
-emmeans(lai_lm, ~ plantingfac)
+emmeans(lai_lm, ~ plantingfac) ## planting date 2 > 1
+cld(emmeans(lai_lm, ~ variety, at = list(plantingfac = '1'))) ## Atlantic and Burbank highest
+cld(emmeans(lai_lm, ~ variety, at = list(plantingfac = '2'))) ## Atlantic is high
+cld(emmeans(lai_lm, ~ variety))
 cld(emmeans(lai_lm, ~ variety * plantingfac))
 
+## sla
+sla_raw = read.csv('../data/springlake/sla/sla_2019_07_09.csv')
+sla_raw$plot_element1 = sapply(strsplit(as.character(sla_raw$sample), 'p'), "[[", 2)
+sla_raw$plot = as.numeric(sapply(strsplit(as.character(sla_raw$plot_element1), 'r'), "[[", 1))
+sla = left_join(sla_raw, plot_info)
+sla$plantingfac = as.factor(sla$planting)
+sla = subset(sla, variety != 'UNICA' & variety != 'TACNA')
 
+hist(sla$sla)
 
-
+sla_lm = lm(sla ~ variety * plantingfac, data = sla)
+anova(sla_lm)
+cld(emmeans(sla_lm, ~variety)) ## pretty similar
+cld(emmeans(sla_lm, ~plantingfac)) ## higher SLA in early planting date
--- data/springlake/leaf_area/leaf_area_calc_2019_07_09.R
+++ data/springlake/leaf_area/leaf_area_calc_2019_07_09.R
@@ -5,12 +5,15 @@ library(LeafArea)
 ## known.distance = 1
 
 # prepare the target directory that contains example image files
-ex.dir <- "raw_images/2019_07_09/cropped_jpeg"
-ex.dir <- eximg()
-list.files(ex.dir)
+dir <- "raw_images/2019_07_09/cropped_jpeg"
+# ex.dir <- eximg()
+list.files(dir)
 
-areas = run.ij(set.directory = ex.dir, save.image = TRUE, check.image = FALSE, 
-               distance.pixel = 74, known.distance = 1, low.circ = 0)
+areas = run.ij(set.directory = dir, save.image = TRUE, check.image = FALSE, 
+               distance.pixel = 74, known.distance = 1, low.circ = 0, low.size = 0, upper.circ = 1,
+               trim.pixel = 0)
+nrow(areas)
+head(areas)
 
 # write.csv(areas, 'areas_2019_07_09.csv')
 hist(areas$total.leaf.area)
